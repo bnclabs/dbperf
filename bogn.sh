bd9bdc74ef824cd23101c700f4faee279abd4d88
prataprc <prataprc@gmail.com>
2017-12-18 19:49:35 +0530

    bogn: full performance suite pass.

--- bogn.sh
+++ bogn.sh
@@ -3,7 +3,7 @@
 LOAD="-load 1000000 -lsm"
 UPSERTS="-inserts 1000000 -upserts 1000000 -setas all"
 DELETES="-deletes 1000000 -delas all"
-READS="-gets 10000000 -getas view -ranges 100000000 -rngas vyn"
+READS="-gets 10000000 -getas all -ranges 100000000 -rngas all"
 rm -rf *.svg dbperf; go build
 
 echo "./dbperf -db bogn -klen 32 -vlen 32 $LOAD $UPSERTS $DELETES $READS"
