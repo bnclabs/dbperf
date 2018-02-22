e07a732f01ea0f7e295bcb2d14551773f5c5e6ab
prataprc <prataprc@gmail.com>
2018-02-22 21:30:30 +0530

    fix script to echo command like args.

--- mod_bubt.sh
+++ mod_bubt.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 LOAD="-load 1000000"
-READS="-gets 100000000 -getas all -ranges 10000000 -rngas all"
+READS="-gets 20000000 -getas all -ranges 10000000 -rngas all"
 
-go build
+rm dbperf; go build
 
+echo "./dbperf -db bubt -klen 32 -vlen 32 $LOAD $READS"
 ./dbperf -db bubt -klen 32 -vlen 32 $LOAD $READS
 go tool pprof -svg dbperf dbperf.pprof  > pprof.svg
 go tool pprof -alloc_space -svg dbperf dbperf.mprof  > alloc_space.svg
