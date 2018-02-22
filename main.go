15f1d89f3f96c3b313eadd531745c5faa068b3de
prataprc <prataprc@gmail.com>
2018-02-22 21:29:40 +0530

    command line option for component logging bnclabs/gostore#64.

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import "os"
 import "flag"
 import "time"
+import "strings"
 import "runtime"
 import "net/http"
 import "runtime/pprof"
 import _ "net/http/pprof"
 
 import "github.com/bnclabs/golog"
+import "github.com/bnclabs/gostore/bogn"
+import "github.com/bnclabs/gostore/bubt"
+import "github.com/bnclabs/gostore/llrb"
 
 var options struct {
 	db       string
@@ -31,6 +35,7 @@ var options struct {
 	delas    string
 	getas    string
 	rngas    string
+	log      string
 }
 
 func optparse(args []string) {
@@ -57,10 +62,26 @@ func optparse(args []string) {
 	f.StringVar(&options.delas, "delas", "all", "del|txn|cur|delcur|all")
 	f.StringVar(&options.getas, "getas", "all", "get|txn|view|all")
 	f.StringVar(&options.rngas, "rngas", "all", "tgn|tyn|vgn|vyn|all")
+	f.StringVar(&options.log, "log", "", "llrb,mvcc,bubt,bogn")
 	f.Parse(args)
 
 	if options.seed == 0 {
 		options.seed = int(time.Now().UnixNano())
+	}
+
+	for _, comp := range strings.Split(options.log, ",") {
+		switch comp {
+		case "bubt":
+			bubt.LogComponents("self")
+		case "bogn":
+			bogn.LogComponents("self")
+		case "llrb", "mvcc":
+			llrb.LogComponents("self")
+		case "all":
+			bubt.LogComponents("all")
+			bogn.LogComponents("all")
+			llrb.LogComponents("all")
+		}
 	}
 }
 
