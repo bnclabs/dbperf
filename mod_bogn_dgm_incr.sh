ba72546f4768a707d122f880fd17d7e536793e7a
prataprc <prataprc@gmail.com>
2018-04-18 21:04:47 +0530

    enable lsm by default, use set & del & get op by default.
    add performance run with value-size for mod_bogn_dgm_incr.sh.

--- mod_bogn_dgm_incr.sh
+++ mod_bogn_dgm_incr.sh
@@ -8,6 +8,16 @@ LOAD="-load 1000000"
 WRITES="-inserts 10000000 -upserts 10000000 -deletes 10000000"
 READS="-gets 10000000 -ranges 10000000"
 echo "./dbperf $ARGS $LOAD $WRITES $READS"
-./dbperf $ARGS $LOAD $WRITES
+./dbperf $ARGS $LOAD $WRITES 
+go tool pprof -svg dbperf dbperf.pprof  > pprof.svg
+go tool pprof -alloc_space -svg dbperf dbperf.mprof  > alloc_space.svg
+
+echo "###### initial build, small value, with value log ###########"
+ARGS="-db bogn -bogn dgm -klen 22 -vlen 128 -vsize 12288 -memcap 4096 -log bogn"
+LOAD="-load 1000000"
+WRITES="-inserts 10000000 -upserts 10000000 -deletes 10000000"
+READS="-gets 10000000 -ranges 10000000"
+echo "./dbperf $ARGS $LOAD $WRITES $READS"
+./dbperf $ARGS $LOAD $WRITES $READS
 go tool pprof -svg dbperf dbperf.pprof  > pprof.svg
 go tool pprof -alloc_space -svg dbperf dbperf.mprof  > alloc_space.svg
